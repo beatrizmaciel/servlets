b138ea98c7437681e3150569ad709dbe4f8a8c9c
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-01-13 12:40:51 -0300

    servlet init

--- gerenciador/src/main/java/br/com/bibs/gerenciador/servlet/Hello.java
+++ gerenciador/src/main/java/br/com/bibs/gerenciador/servlet/Hello.java
@@ -1,16 +1,28 @@
 package br.com.bibs.gerenciador.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//oi			urlPatterns = nome do servlet
+@WebServlet(urlPatterns="/bibs-server")
 public class Hello extends HttpServlet {
 	
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp){
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		// getWriter = dados textuais || get OutputStream = conteúdo binário
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Olá, mundo Servlet.");
+		out.println("</body>");
+		out.println("</html>");
 	}
 	
 
